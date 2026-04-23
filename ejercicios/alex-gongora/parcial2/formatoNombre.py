a42aa113527912dbf8c4aa4305a6c963dfc4587e
Alex Gongora <alexander.gongora_pm25241@cecyte.edu.mx>
2026-04-23 10:49:20 -0600

    Se agregaron los cambios en sistemaCobro.py

---
+++ ejercicios/alex-gongora/parcial2/formatoNombre.py
@@ -0,0 +1,20 @@
+def formato_nombre(name, lastName):
+    """
+    Devuelve el nombre de usuario en el formato Apellido, Nombre.
+
+    Argumentos:
+    name (str): Primer nombre del usuario
+    lastName (str): Apellido paterno del usuario.
+
+    Retorno:
+    (str): El nobre completo en formato Apellido, Nombre.
+    """
+    return f"{lastName}, {name}"
+
+#Funcion main para ejecucion del codigo.
+_name = input("Introduce tu primer apellido: ")
+_lastName = input("Introduce tu apellido paterno: ")
+print(formato_nombre(_name,_lastName))
+
+if __name__== "__main__":
+    main()
