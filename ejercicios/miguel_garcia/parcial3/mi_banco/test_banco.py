58c1d5e1665cb7dea185ded656e383184ef43d9c
miguel garcia <jose.garcia_pm25262@cecyte.edu.mx>
2026-05-21 03:52:26 -0600

    trabajos del parcial

--- ejercicios/miguel_garcia/parcial3/mi_banco/test_banco.py
+++ ejercicios/miguel_garcia/parcial3/mi_banco/test_banco.py
@@ -3,13 +3,22 @@ import unittest
 from cuenta import Cuenta
 from banco import banco
 
-class testintegracionbanco(unittest.TestCase):
+class TestIntegracionBanco(unittest.TestCase):
+
     def setUp(self):
-        self.cuenta1 = cuenta("fulanito perez","001",1000)
-        self.cuenta2 = cuenta("Perecilia Sanchez","002")
+        self.cuenta1 = Cuenta("Fulanito Perez", "001", 1000)
+        self.cuenta2 = Cuenta("Perezcils Sanchez", "002")
 
-        self.banco =self.banco()
+        self.banco = banco()
 
+    def Test_transferencia_exitosa(self):
+        resultado = self.banco.trasferir(self.cuenta1, self.cuenta2, 350)
+        self.assertTrue(resultado, "Deberia realisarse de manera correcta la transferencia")
+        self.assertEqual(self.cuenta1.saldo, 650, "El saldo de la cuenta 1 deberia ser 650")
+        self.assertEqual(self.cuenta2.saldo, 350, "el saldo de la cuenta 2 deberia ser 350")
 
-    def test_tranferencia_exitosa(self):
-        resultado = self.banco.transferir(self.cuenta1, self.cuenta2, 350)
+    def test_transferencia_saldo_insufuciente(self):
+        resultado = self.banco.trasferir(self.cuenta1, self.cuenta2, 1200)
+        self.assertFalse(resultado, "la transferencia no se deberia realizar al no disponer")
+        self.assertEqual(self.cuenta1.saldo, 1000, "El saldodeberia mantenerse sin cambios")
+        self.assertEqual(self.cuenta2.saldo, 0, "El saldo de la cuenta 2 deberia ser 0")
